b600d6c6a06cefeb97c8fb66b0f99997e4ca6867
zhanghongy <865383353@qq.com>
2019-06-17 09:34:42 +0800

    update

---
+++ Fragment/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_left);
+        button.setOnClickListener(this);
+
+        replaceFragment(new RightFragment());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_left:
+                replaceFragment(new AnotherRightFragment());
+                break;
+                default:break;
+        }
+
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        //模拟添加到返回栈
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+}
