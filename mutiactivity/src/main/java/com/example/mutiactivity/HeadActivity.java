2b85574e5a7f298a92eaf60e5b0944aa7ab4e763
小老弟 <17521188586@163.com>
2019-06-26 22:44:25 +0800

    update

--- mutiactivity/src/main/java/com/example/mutiactivity/HeadActivity.java
+++ mutiactivity/src/main/java/com/example/mutiactivity/HeadActivity.java
@@ -1,13 +1,76 @@
 package com.example.mutiactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class HeadActivity extends AppCompatActivity {
 
+    int[] pics = new int[]{
+            R.drawable.boy,
+            R.drawable.girl,
+            R.drawable.man,
+            R.drawable.user
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_head);
+        GridView gridView = findViewById(R.id.gridview);
+        BaseAdapter adapter = new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return pics.length;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                ImageView imageView;
+                if(view == null){
+                    imageView = new ImageView(HeadActivity.this);
+                   // imageView.setAdjustViewBounds(true);
+//                    imageView.setMaxHeight(150);
+//                    imageView.setMaxWidth(158);
+//                    imageView.setPadding(5,5,5,5);
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                }else {
+                    imageView = (ImageView) view;
+                }
+                return imageView;
+            }
+        };
+
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = getIntent();
+                Bundle bundle = new Bundle();
+                bundle.putInt("imageId",pics[i]);
+                //intent.putExtra("imageId",pics[i]);
+                intent.putExtras(bundle);
+                setResult(1,intent);
+                finish();
+
+
+            }
+        });
+
     }
 }
--- mutiactivity/src/main/java/com/example/mutiactivity/MainActivity.java
+++ mutiactivity/src/main/java/com/example/mutiactivity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mutiactivity;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HeadActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1&&requestCode==1){
+            Bundle extras = data.getExtras();
+            int imageId = extras.getInt("imageId");
+            ImageView imageView = findViewById(R.id.head);
+            imageView.setImageResource(imageId);
+
+        }
     }
 }
