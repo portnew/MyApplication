4426fa37dc9e76be1369403706ddd22c453fb488
zhanghongy <865383353@qq.com>
2019-06-28 16:34:07 +0800

    update

---
+++ shareepreferences/src/main/java/com/example/shareepreferences/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.shareepreferences;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.save_data);
+        Button btn2 = findViewById(R.id.restore_data);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age",18);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getSharedPreferences("data",MODE_PRIVATE);
+                String name = preferences.getString("name","");
+                int age = preferences.getInt("age",0);
+                boolean married = preferences.getBoolean("married",true);
+                Log.d("Restore Data+++++++++++++++++++++",name);
+                Log.d("Restore Data+++++++++++++++++++++",age+"");
+                Log.d("Restore Data+++++++++++++++++++++",married+"");
+            }
+        });
+    }
+}
---
+++ sqlite/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  MyDatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,1);
+        Button button = findViewById(R.id.create_database);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+    }
+}
