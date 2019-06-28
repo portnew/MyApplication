58e96f75f43bf10e6b4582699a76dde7dc00febf
zhanghongy <865383353@qq.com>
2019-06-28 11:01:40 +0800

    update

---
+++ wechat/src/main/java/com/example/wechat/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.wechat;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3,btn4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragment = manager.findFragmentById(R.id.fragment_base);
+//                transaction.replace(R.id.fragment_base, new Fragment_2());
+                transaction.remove(fragment);
+                transaction.commit();
+            }
+        });
+//        OnClick onClick = new OnClick();
+//        btn1.setOnClickListener(onClick);
+//        btn2.setOnClickListener(onClick);
+//        btn3.setOnClickListener(onClick);
+//        btn4.setOnClickListener(onClick);
+
+    }
+        /*class OnClick implements View.OnClickListener {
+
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment fragment = null;
+                switch (view.getId()) {
+                    case R.id.btn1:
+                        fragment = new Fragment_1();
+                        break;
+                    case R.id.btn2:
+                        fragment = new Fragment_2();
+
+                        break;
+                    case R.id.btn3:
+                        fragment = new Fragment_3();
+
+                        break;
+                    case R.id.btn4:
+                        fragment = new Fragment_4();
+
+                        break;
+                    default:
+                        break;
+                }
+                transaction.replace(R.id.fragment_base, fragment);
+                transaction.commit();
+            }
+        }*/
+    }
+
