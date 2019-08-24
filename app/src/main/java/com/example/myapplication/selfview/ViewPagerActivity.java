8d843256ebe75930cdaa41f877f3b8357446d554
zhanghongy <865383353@qq.com>
2019-08-24 17:47:49 +0800

    update

--- app/src/main/java/com/example/myapplication/selfview/ViewPagerActivity.java
+++ app/src/main/java/com/example/myapplication/selfview/ViewPagerActivity.java
@@ -1,15 +1,49 @@
 package com.example.myapplication.selfview;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+
 public class ViewPagerActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TextView textView;
+    private LinearLayout layout;
+    private ArrayList<ImageView>  list;
+    // 图片资源ID
+    private final int[] imageIds = {
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e };
+    // 图片标题集合
+    private final String[] imageDescriptions = {
+            "尚硅谷波河争霸赛！",
+            "凝聚你我，放飞梦想！",
+            "抱歉没座位了！",
+            "7月就业名单全部曝光！",
+            "平均起薪11345元"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+
+        viewPager = findViewById(R.id.viewpager);
+        textView = findViewById(R.id.text_viewbottom);
+        layout = findViewById(R.id.layout_point);
+
+
+        list = new ArrayList<>();
+
+
     }
 }
